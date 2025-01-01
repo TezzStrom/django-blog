8744ba88a283561e1aa61fcd1517b3fe00a40e99
Tezz Strömberg <kzefyowh@students.codeinstitute.net>
2025-01-01 16:54:07 +0000

    Building the homepage with Django generic views

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import welcome_message
+from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', welcome_message, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
